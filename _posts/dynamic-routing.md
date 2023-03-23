10140d3262cdde3b0b1813fac3a692bd725bae2c
forestheims <forestheims@gmail.com>
2023-03-23 12:49:35 -0700

    a place we exist

---
+++ _posts/dynamic-routing.md
@@ -0,0 +1,13 @@
+---
+title: ""
+excerpt: ""
+coverImage: ""
+date: "2023"
+author:
+  name: ""
+  picture: ""
+ogImage:
+  url: ""
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.
---
+++ components/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className="flex row gap-4 justify-end">
+    <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <a href="https://forestheims.net/" className="hover:underline">
+        Home
+      </a>
+    </h2>
+    <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <a href="https://forestheims.dev/" className="hover:underline">
+        Blog
+      </a>
+    </h2>
+    <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <a href="https://forestheims.org/" className="hover:underline">
+        Projects
+      </a>
+    </h2>
+    <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <a href="https://forestheims.com/" className="hover:underline">
+        Contact
+      </a>
+    </h2>
+    </header>
+  )
+}
+
+export default Header

2289a648602688aaddb6e22cf394b1b9fb766110
forestheims <forestheims@gmail.com>
2023-03-26 00:20:31 -0700

    add umami analytics script

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 const Meta = () => {
@@ -37,6 +38,7 @@ const Meta = () => {
         content="Home page for Forest Heims - Software Engineer & Chemist"
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <Script async defer src="https://analytics.umami.is/script.js" data-website-id="418b0070-dbc2-480f-a9de-b852adcf3de6" />
     </Head>
   )
 }

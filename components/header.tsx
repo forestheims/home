49e127648161c9e1f0b4dc3d463f8a52503f670e
forestheims <forestheims@gmail.com>
2023-04-07 13:51:19 -0700

    add refferrer to links

--- components/header.tsx
+++ components/header.tsx
@@ -9,22 +9,22 @@ const Header = () => {
     <div className="flex row gap-4 justify-between"> */}
 
     <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <a href="https://forestheims.net/" className="hover:underline">
+      <a href="https://forestheims.net/" className="hover:underline" referrerPolicy="origin">
         Home
       </a>
     </h2>
     <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <a href="https://forestheims.dev/" className="hover:underline">
+      <a href="https://forestheims.dev/" className="hover:underline" referrerPolicy="origin">
         Blog
       </a>
     </h2>
     <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <a href="https://forestheims.org/" className="hover:underline">
+      <a href="https://forestheims.org/" className="hover:underline" referrerPolicy="origin">
         Projects
       </a>
     </h2>
     <h2 className="text-2xl md:text-2xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <a href="https://forestheims.com/" className="hover:underline">
+      <a href="https://forestheims.com/" className="hover:underline" referrerPolicy="origin">
         Contact
       </a>
     </h2>
